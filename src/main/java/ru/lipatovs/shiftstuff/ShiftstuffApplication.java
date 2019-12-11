b6b7024ab1636b72cefa5330c1e39c40a13262ff
fishtrack <ysJcC6E6gzH69>
2019-12-11 17:52:23 +0300

    Добавил в проект недостающие файлы: pom.xml, attribute and etc

---
+++ src/main/java/ru/lipatovs/shiftstuff/ShiftstuffApplication.java
@@ -0,0 +1,13 @@
+package ru.lipatovs.shiftstuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftstuffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiftstuffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lipatovs/shiftstuff/ShiftstuffApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lipatovs.shiftstuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftstuffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
